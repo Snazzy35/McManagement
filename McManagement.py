8f28b033cd6d16d3edce86910f4b309fc31a2258
Snazzy35 <69607296+Snazzy35@users.noreply.github.com>
2024-06-09 15:03:54 -0400

    Fix wget commands

--- McManagement.py
+++ McManagement.py
@@ -41,31 +41,31 @@ if args.docs:
     subprocess.run(['cat', 'readme.md'])
 def install_paper(MCV, MCP):
     if MCV == 8:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar"])
     elif MCV == 9:   
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.9.4/builds/775/downloads/paper-1.9.4-775.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.9.4/builds/775/downloads/paper-1.9.4-775.jar"])
     elif MCV == 10:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.10.2/builds/918/downloads/paper-1.10.2-918.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.10.2/builds/918/downloads/paper-1.10.2-918.jar"])
     elif MCV == 11:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.11.2/builds/1106/downloads/paper-1.11.2-1106.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.11.2/builds/1106/downloads/paper-1.11.2-1106.jar"])
     elif MCV == 12:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"])
     elif MCV == 13:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.13.2/builds/657/downloads/paper-1.13.2-657.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.13.2/builds/657/downloads/paper-1.13.2-657.jar"])
     elif MCV == 14:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/245/downloads/paper-1.14.4-245.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/245/downloads/paper-1.14.4-245.jar"])
     elif MCV == 15:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar"])
     elif MCV == 16:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar"])
     elif MCV == 17:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar"])
     elif MCV == 18:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar"])
     elif MCV == 19:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.19.4/builds/550/downloads/paper-1.19.4-550.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.19.4/builds/550/downloads/paper-1.19.4-550.jar"])
     elif MCV == 20:
-        subprocess.run(["wget", "-P", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/134/downloads/paper-1.20.6-134.jar"])
+        subprocess.run(["wget", "-O", MCP, "https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/134/downloads/paper-1.20.6-134.jar"])
     else:
         print('Unknown MC Version. Check your MC number.')
 McMemory = args.mcmemory
